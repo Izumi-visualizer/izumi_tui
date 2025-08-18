4a1cb7be9c6443e97f871afb9d285c7c3af53dd4
Nara Díaz Viñolas <nara.diaz@bsc.es>
2025-08-18 16:34:49 +0200

    Initial commit (copy from Izumi-visualizer/izumi)

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "A multi-format instruction pipeline dump visualizer based on Konata";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    
+    libizumi.url = "github:Izumi-visualizer/libizumi";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+      perSystem = { self', pkgs, inputs', ... }: {
+        packages.izumi_tui = pkgs.callPackage ./default.nix {
+          libizumi = inputs'.libizumi.packages.default;
+        };
+        packages.default = self'.packages.izumi_tui;
+        
+        checks = self'.packages.default.tests;
+      };
+    };
+}
